15ef3e867c77675254aaeebf76b1b6ff03c4921c
Ethan Saul <saul.ethan516@gmail.com>
2017-08-28 14:13:20 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array]
+}
+function destructivelyAddElementToBeginningOfArray (array, foo) {
+  return array.unshift(foo)
+}
+function addElementToEndOfArray (array, element) {
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray (array, element) {
+  return array.push(element)
+}
+function accessElementInArray (array, element) {
+  return(array [element])
+}
+function destructivelyRemoveElementFromBeginningOfArray (array,) {
+  array.shift()
+
+}
